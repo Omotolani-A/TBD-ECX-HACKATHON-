a432fcd0aa97d891532adf2cd10e8f9fec3fbbec
Omotolani Adekunle <omotolaniadekunle14@gmail.com>
2025-07-23 22:32:12 +0000

    Add config, models and set up MongoDB connection

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Connect to DB
+connectDB();
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('Claimr is live!');
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
