152fd524102cbdddff7a51ccd70990d854fb6cfe
Omotolani Adekunle <omotolaniadekunle14@gmail.com>
2025-07-28 04:10:09 +0100

    Update userControllers.js

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -74,3 +74,21 @@ export const loginUser = async (req, res) => {
   }
 };
 
+
+
+export const getUserProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.status(200).json({
+      message: 'User profile fetched successfully',
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error: error.message });
+  }
+};
