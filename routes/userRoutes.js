e5dbc697d9b55589b09e668c7dbcfc600d191b64
Omotolani Adekunle <omotolaniadekunle14@gmail.com>
2025-07-29 01:48:47 +0100

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { registerUser, loginUser, getUserProfile } from '../controllers/userControllers.js';
+import { registerUser, loginUser, getUserProfile, updateUserProfile } from '../controllers/userControllers.js';
 import protect from '../middleware/authMiddleware.js';
 
 const router = express.Router();
@@ -9,5 +9,7 @@ router.post('/register', registerUser);
 router.post('/login', loginUser);
 
 router.get('/profile', protect, getUserProfile);
+
+router.put('/profile', protect, updateUserProfile);
  
 export default router;
