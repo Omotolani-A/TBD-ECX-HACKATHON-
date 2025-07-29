f59b0daf7e85bb636d476d016a57c841132c710d
Omotolani Adekunle <omotolaniadekunle14@gmail.com>
2025-07-29 03:49:52 +0100

    Update paymentRoutes.js

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,8 +1,11 @@
 import express from 'express';
-import { initiatePayment } from '../controllers/paymentControllers.js';
+import { initiatePayment, verifyPayment } from '../controllers/paymentControllers.js';
+import protect from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/initiate', initiatePayment);
+
+router.get('/verify', protect, verifyPayment);
 
 export default router;
